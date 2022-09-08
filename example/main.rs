a22d90b25d6d6068f4a96242f092ab1ebcf971f8
xiaoyuanxun <53613219+xiaoyuanxun@users.noreply.github.com>
2022-09-08 15:00:17 +0800

    refresh

--- example/main.rs
+++ example/main.rs
@@ -2,7 +2,7 @@ use lib::isp_sdk;
 
 #[tokio::main]
 async fn main() {
-    let pem_identity_path = "/Users/heyuanxun/Documents/GitHub/ISP-SDK/identity.pem";
+    let pem_identity_path = "identity.pem";
     // let ans = isp_sdk::get_isp_admins(pem_identity_path).await;
     // println!("{:?}", &ans[0].to_text());
     let ans = isp_sdk::get_sub_account(pem_identity_path).await;
